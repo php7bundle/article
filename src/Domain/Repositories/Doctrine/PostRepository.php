50e2844637ca192e3d670b6c98109fc5da86e0db
Vitaliy <theyamshikov@yandex.ru>
2020-03-10 00:12:00 +0600

    feat: make doctrine

--- src/Domain/Repositories/Doctrine/PostRepository.php
+++ src/Domain/Repositories/Doctrine/PostRepository.php
@@ -1,23 +1,27 @@
 <?php
 
-namespace PhpBundle\Article\Domain\Repositories\Eloquent;
+namespace PhpBundle\Article\Domain\Repositories\Doctrine;
 
+use Doctrine\DBAL\Connection;
 use PhpBundle\Article\Domain\Entities\PostEntity;
 use PhpBundle\Article\Domain\Interfaces\CategoryRepositoryInterface;
 use PhpBundle\Article\Domain\Interfaces\PostRepositoryInterface;
 use PhpBundle\Article\Domain\Interfaces\TagPostRepositoryInterface;
 use PhpBundle\Article\Domain\Interfaces\TagRepositoryInterface;
 use PhpBundle\Article\Domain\Repositories\Relations\PostRelation;
-use PhpLab\Eloquent\Db\Base\BaseEloquentCrudRepository;
-use PhpLab\Eloquent\Db\Helpers\Manager;
+use PhpLab\Eloquent\Db\Base\BaseDoctrineCrudRepository;
 
-class PostRepository extends BaseEloquentCrudRepository implements PostRepositoryInterface
+class PostRepository extends BaseDoctrineCrudRepository implements PostRepositoryInterface
 {
 
     protected $tableName = 'article_post';
+    private $categoryRepository;
+    private $tagPostRepository;
+    private $tagRepository;
+
     private $relation;
 
-    public function __construct(Manager $capsule, PostRelation $postRelation)
+    public function __construct(Connection $capsule, PostRelation $postRelation)
     {
         parent::__construct($capsule);
         $this->relation = $postRelation;
--- src/Symfony/Api/ArticleModule.php
+++ src/Symfony/Api/ArticleModule.php
@@ -2,43 +2,61 @@
 
 namespace PhpBundle\Article\Symfony\Api;
 
+use Doctrine\DBAL\Connection;
+use Illuminate\Container\Container;
+use PhpBundle\Article\Domain\Interfaces\CategoryRepositoryInterface;
+use PhpBundle\Article\Domain\Interfaces\PostRepositoryInterface;
+use PhpBundle\Article\Domain\Interfaces\PostServiceInterface;
+use PhpBundle\Article\Domain\Interfaces\TagPostRepositoryInterface;
+use PhpBundle\Article\Domain\Interfaces\TagRepositoryInterface;
+use PhpBundle\Article\Domain\Repositories\Relations\PostRelation;
+use PhpLab\Eloquent\Db\Helpers\DoctrineHelper;
 use PhpLab\Rest\Helpers\RestApiControllerHelper;
 use PhpBundle\Article\Symfony\Api\Controllers\ArticleController;
 use PhpBundle\Article\Domain\Repositories\Eloquent\CategoryRepository;
-use PhpBundle\Article\Domain\Repositories\Eloquent\PostRepository;
+use PhpBundle\Article\Domain\Repositories\Doctrine\PostRepository;
 use PhpBundle\Article\Domain\Repositories\Eloquent\TagPostRepository;
 use PhpBundle\Article\Domain\Repositories\Eloquent\TagRepository;
 use PhpBundle\Article\Domain\Services\PostService;
 use PhpLab\Eloquent\Db\Helpers\Manager;
+use Psr\Container\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 class ArticleModule
 {
 
-    private $capsule;
-
     public function __construct() {
         // init DB
-        $this->capsule = new Manager(null, $_ENV['ELOQUENT_CONFIG_FILE']);
+        //$this->capsule = new Manager(null, $_ENV['ELOQUENT_CONFIG_FILE']);
+        //$this->doctrineConnection = DoctrineHelper::createConnection();
     }
 
     public function run() {
-        // create service
-        $categoryRepository = new CategoryRepository($this->capsule);
-        $tagRepository = new TagRepository($this->capsule);
-        $tagPostRepository = new TagPostRepository($this->capsule);
-        $postRepository = new PostRepository($this->capsule, $categoryRepository, $tagRepository, $tagPostRepository);
-        $postService = new PostService($postRepository);
+        $container = $this->makeContainer();
+        $postService = $container->get(PostService::class);
 
         // define routes
         $routes = RestApiControllerHelper::defineCrudRoutes('v1/article-post', ArticleController::class);
         $request = Request::createFromGlobals();
 
-        $controllers = [
-            ArticleController::class => new ArticleController($postService),
-        ];
-        $response = RestApiControllerHelper::runAll($request, $routes, $controllers);
+        $response = RestApiControllerHelper::runAll($request, $routes, $container);
         $response->send();
+    }
+
+    private function makeContainer(): ContainerInterface
+    {
+        $container = Container::getInstance();
+        $container->bind(Manager::class, Manager::class, true);
+        $container->bind(Connection::class, function() {
+            return DoctrineHelper::createConnection();
+        }, true);
+        $container->bind(Manager::class, \PhpLab\Eloquent\Db\Helpers\Manager::class, true);
+        $container->bind(CategoryRepositoryInterface::class, CategoryRepository::class, true);
+        $container->bind(TagRepositoryInterface::class, TagRepository::class, true);
+        $container->bind(TagPostRepositoryInterface::class, TagPostRepository::class, true);
+        $container->bind(PostRepositoryInterface::class, PostRepository::class, true);
+        $container->bind(PostServiceInterface::class, PostService::class, true);
+        return $container;
     }
 
 }
